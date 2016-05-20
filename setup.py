571a3289aae36eb222662b1d15490c65fc962b76
Robert Veznaver <r.veznaver@criteo.com>
2016-05-20 13:44:47 +0200

    release v0.5.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='pgwrr',
-      version='0.5',
+      version='0.5.1',
       description='PowerDNS GeoIP Weighted Round Robin pipe backend plugin',
       url='https://github.com/criteo/pgwrr',
       author='Robert Veznaver',
